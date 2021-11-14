0d28004aa2b1cafc2ef463918b2e4a069e3fdc3b
Santiago Pantoja <camilolp16@gmail.com>
2021-11-13 21:12:53 -0500

    commit para editando formulario

--- enviar.php
+++ enviar.php
@@ -1,5 +1,11 @@
 <?php
 
+
+if  (isset($_POST["solicitar"])){
+
+
+
+
 //validamos datos del servidor
 $user = "root";
 $pass = "";
@@ -13,23 +19,33 @@ $NOMBRE = $_POST["nombre"] ;
 $CORREO = $_POST["correo"];
 $CELULAR = $_POST["celular"] ;
 $DIRECCION = $_POST["direccion"] ;
-$CAJA = $_POST["caja"] ;
+$TAMALES = $_POST["tamales"] ;
+$QUIMBOLITOS= $_POST["quimbolitos"] ; 
 
 
 //verificamos la conexion a base datos
-if(!$connection) 
+/* if(!$connection) 
         {
             echo "No se ha podido conectar con el servidor" . mysql_error();
         }
   else
         {
             echo "<b><h3>Hemos conectado al servidor</h3></b>" ;
-        }
+        } */
         //indicamos el nombre de la base datos
         $datab = "basededatos";
         //indicamos selecionar ala base datos
         $db = mysqli_select_db($connection,$datab);
 
+        $errores=array();
+
+         if (isset($_POST['nombre'])) // validando que correo y clave tengan caracteres 
+{
+    $NOMBRE=$_POST["nombre"];
+    if ($NOMBRE=="") {
+        echo "mal";
+    }
+}
         if (!$db)
         {
         echo "No se ha podido encontrar la Tabla";
@@ -39,51 +55,20 @@ if(!$connection)
         echo "<h3>Tabla seleccionada:</h3>" ;
         }
         //insertamos datos de registro al mysql xamp, indicando nombre de la tabla y sus atributos
-        $instruccion_SQL = "INSERT INTO `pedidos`(`nombre`, `correo`, `celular`, `direccion`, `caja`)
-        VALUES ('[$NOMBRE]','[$CORREO]','$CELULAR','$DIRECCION','$CAJA')";
+        $instruccion_SQL = "INSERT INTO `pedidos`(`nombre`, `correo`, `celular`, `direccion`, `tamales`, `quimbolitos`)
+        VALUES ('[$NOMBRE]','[$CORREO]','$CELULAR','$DIRECCION','$TAMALES','$QUIMBOLITOS')";
                            
                             
         $resultado = mysqli_query($connection,$instruccion_SQL);
 
-        //$consulta = "SELECT * FROM tabla where id ='2'"; si queremos que nos muestre solo un registro en especifivo de ID
-       /*  $consulta = "SELECT * FROM pedidos ";
-        
-$result = mysqli_query($connection,$consulta);
-if(!$result) 
-{
-    echo "No se ha podido realizar la consulta";
-}
-echo "<table>";
-echo "<tr>";
 
-echo "<th><h1>Nombre</th></h1>";
-echo "<th><h1>correo</th></h1>";
-echo "<th><h1>direccion</th></h1>";
-echo "<th><h1>celular</th></h1>";
-echo "<th><h1>telefono</th></h1>";
 
-echo "</tr>";
 
-while ($colum = mysqli_fetch_array($result))
- {
-    echo "<tr>";
-   
-    echo "<td><h2>" . $colum['nombre']. "</td></h2>";
-    echo "<td><h2>" . $colum['correo']. "</td></h2>";
-    echo "<td><h2>" . $colum['direccion']. "</td></h2>";
-    echo "<td><h2>" . $colum['celular']. "</td></h2>";
-    echo "<td><h2>" . $colum['telefono']. "</td></h2>";
 
-   
-    echo "</tr>";
-}
-echo "</table>";
 
-mysqli_close( $connection );
 
-   //echo "Fuera " ;
-   echo'<a href="index.html"> Volver AtrÃ¡s</a>';
- */
+
+} 
 
 ?>
 
